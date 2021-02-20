d5fdcd0c1f1defcd85404c2c83a2ff16bbbee64f
Chris Wiederspan <chris@wiederspan.com>
2021-02-20 10:03:41 -0700

    Latent changes

---
+++ src/ServiceTracker.Ui/Models/BingMaps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceTracker.Models {
+
+    public class Location {
+
+        public double latitude { get; set; }
+
+        public double longitude { get; set; }
+
+        public Location(double lat, double lon) {
+            this.latitude = lat;
+            this.longitude = lon;
+        }
+    }
+
+    public class Pushpin {
+
+        public string color { get; set; }
+
+        public Location location { get; set; }
+    }
+}
